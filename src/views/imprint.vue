cd7ad25d84798020309fb897bf0e6d8033184f99
cmauf <constantin.mauf@outlook.de>
2021-04-24 15:08:45 +0200

    Added working dist, removed imprint/privacy

--- src/views/imprint.vue
+++ src/views/imprint.vue
@@ -16,48 +16,6 @@ Dependencies: @/i18n/imprint
           <div style="height: 2rem"></div>
           <h1><b>{{ i18n.title }}</b></h1>
 
-          <div style="height: 1rem"></div>
-
-          <p>ARBEIT UND LEBEN - DGB/VHS, Landesarbeitsgemeinschaft Berlin e.V.<br>
-            Fachbereich Arbeit &amp; Qualifizierung<br>
-            Projekt WayIn – Der Inklusionswegweiser für Arbeitgeber: Ein digitales Informationsangebot zur Beschäftigung von Menschen mit Behinderung<br>
-            Kapweg 4<br>
-            13405 Berlin<br>
-            Telefon: (030) 5130 192 21<br>
-            E-Mail: inklusion@berlin.arbeitundleben.de<br>
-            GESCHÄFTSFÜHRUNG<br>
-            Dr. Undine Flemming<br>
-            Michael Lüdtke</p>
-          <p>VERTRETUNGSBERECHTIGTER VORSTAND<br>
-            Doro Zinke (Vorsitzende)<br>
-            Knut Schiemann (stellv. Vorsitzender)<br>
-            Registergericht: Amtsgericht Charlottenburg<br>
-            Registernummer: VR 12205</p>
-          <p>UMSATZSTEUER-IDENTIFIKATIONSNUMMER<br>
-            gemäß § 27 a Umsatzsteuergesetz<br>
-            DE 27 / 660 / 52189</p>
-          <p>PARTNER UND TRÄGER<br>
-            Europäischer Sozialfond<br>
-            Bundesministerium für Bildung und Forschung<br>
-            Humboldt Universität zu Berlin<br>
-            KOPF, HAND + FUSS gemeinnützige Gesellschaft für Bildung mbH</p>
-          <p>KONZEPTION, GESTALTUNG UND UMSETZUNG DER WEBSITE</p>
-          <p>Max Meisenheimer</p>
-          <p>INHALTLICH VERANTWORTLICHER<br>
-            gemäß § 10 Absatz 3 MDStV<br>
-            Dr. Undine Flemming (Anschrift wie oben)<br>
-            Michael Lüdtke (Anschrift wie oben)<br>
-            BILDNACHWEISE<br>
-            Urheberrechte an den Bildern auf der Startseite bei:<br>
-            © XXXXX<br>
-            HINWEISE AN RECHTEINHABER<br>
-            Die Herausgeber weisen darauf hin, kein Material zu verwenden, das Urheberrechten Dritter<br>
-            unterliegt. Bei der sehr großen Zahl der in deutscher Sprache vorliegenden elektronischen und vor<br>
-            allem schriftlichen Publikationen kann er aber nicht ausschließen, dass von Benutzern dennoch<br>
-            Material eingebracht wird, das bestehende Schutzrechte verletzt, und das nicht sofort als solches<br>
-            erkannt wird. Wenn den Herausgebern eine entsprechende Urheberrechtsverletzung angezeigt wird,<br>
-            wird das betreffende Material umgehend vom Server genommen.</p>
-
         </div>
       </div>
     </div>
---
+++ working_dist/precache-manifest.b1a5e5fe8280f04b05c25fb35f71e473.js
@@ -0,0 +1,494 @@
+self.__precacheManifest = [
+  {
+    "revision": "532b3d7ec1cb9b42c829",
+    "url": "css/app.833b323c.css"
+  },
+  {
+    "revision": "532b3d7ec1cb9b42c829",
+    "url": "js/app.cd2c3514.js"
+  },
+  {
+    "revision": "a773aee009c82142ee79",
+    "url": "css/chunk-vendors.5ab4e916.css"
+  },
+  {
+    "revision": "a773aee009c82142ee79",
+    "url": "js/chunk-vendors.51ba16f1.js"
+  },
+  {
+    "revision": "89ffa3aba80d30ee0a9371b25c968bbb",
+    "url": "img/fa-regular-400.89ffa3ab.svg"
+  },
+  {
+    "revision": "c1868c9545d2de1cf8488f1dadd8c9d0",
+    "url": "fonts/fa-brands-400.c1868c95.eot"
+  },
+  {
+    "revision": "a06da7f0950f9dd366fc9db9d56d618a",
+    "url": "fonts/fa-brands-400.a06da7f0.woff2"
+  },
+  {
+    "revision": "261d666b0147c6c5cda07265f98b8f8c",
+    "url": "fonts/fa-regular-400.261d666b.eot"
+  },
+  {
+    "revision": "ec3cfddedb8bebd2d7a3fdf511f7c1cc",
+    "url": "fonts/fa-brands-400.ec3cfdde.woff"
+  },
+  {
+    "revision": "13685372945d816a2b474fc082fd9aaa",
+    "url": "fonts/fa-brands-400.13685372.ttf"
+  },
+  {
+    "revision": "c20b5b7362d8d7bb7eddf94344ace33e",
+    "url": "fonts/fa-regular-400.c20b5b73.woff2"
+  },
+  {
+    "revision": "f89ea91ecd1ca2db7e09baa2c4b156d1",
+    "url": "fonts/fa-regular-400.f89ea91e.woff"
+  },
+  {
+    "revision": "db78b9359171f24936b16d84f63af378",
+    "url": "fonts/fa-regular-400.db78b935.ttf"
+  },
+  {
+    "revision": "a0369ea57eb6d3843d6474c035111f29",
+    "url": "fonts/fa-solid-900.a0369ea5.eot"
+  },
+  {
+    "revision": "b15db15f746f29ffa02638cb455b8ec0",
+    "url": "fonts/fa-solid-900.b15db15f.woff2"
+  },
+  {
+    "revision": "bea989e82b07e9687c26fc58a4805021",
+    "url": "fonts/fa-solid-900.bea989e8.woff"
+  },
+  {
+    "revision": "1ab236ed440ee51810c56bd16628aef0",
+    "url": "fonts/fa-solid-900.1ab236ed.ttf"
+  },
+  {
+    "revision": "0cb5a5c0d251c109458c85c6afeffbaa",
+    "url": "img/fa-brands-400.0cb5a5c0.svg"
+  },
+  {
+    "revision": "ec763292e583294612f124c0b0def500",
+    "url": "img/fa-solid-900.ec763292.svg"
+  },
+  {
+    "revision": "36b6858eaccd468d33e7ce80c49927af",
+    "url": "img/pl.36b6858e.svg"
+  },
+  {
+    "revision": "b1aff3d0a6b1d60c60ef37f3dea432a9",
+    "url": "img/anmelden.b1aff3d0.svg"
+  },
+  {
+    "revision": "871f72e8a27afc636464b350e3eb79f7",
+    "url": "img/it.871f72e8.svg"
+  },
+  {
+    "revision": "d145b19cd4b4d12532d7797c67e17dae",
+    "url": "img/nl.d145b19c.svg"
+  },
+  {
+    "revision": "871857253ef61b5e38845abafc82ca54",
+    "url": "img/vimeo.87185725.svg"
+  },
+  {
+    "revision": "929b106709e185e0dfd86ba7ade6b866",
+    "url": "img/en.929b1067.svg"
+  },
+  {
+    "revision": "89a5e143852f500ddac5ab4ced3754f9",
+    "url": "img/email.89a5e143.svg"
+  },
+  {
+    "revision": "28140f81ac68f5c4b07e69d1dda53e26",
+    "url": "img/laya-logo-xs.28140f81.png"
+  },
+  {
+    "revision": "bd65e75ee81edd5dbf6726b65a89203e",
+    "url": "img/logo.bd65e75e.svg"
+  },
+  {
+    "revision": "286d411637c9c84ba20f836d17396f1f",
+    "url": "img/twitter.286d4116.svg"
+  },
+  {
+    "revision": "3016500ed0963646448d961a43667898",
+    "url": "img/facebook.3016500e.svg"
+  },
+  {
+    "revision": "0be4e69482311c626186a5767e2711e5",
+    "url": "img/es.0be4e694.svg"
+  },
+  {
+    "revision": "2a1046c73dd5b5bbd31dc94267f98eee",
+    "url": "img/ar.2a1046c7.svg"
+  },
+  {
+    "revision": "cd210ed0e58a2924efc731d5f07bd13a",
+    "url": "img/kurse.cd210ed0.svg"
+  },
+  {
+    "revision": "31a8106a39207a008cd79263badec2c4",
+    "url": "img/de.31a8106a.svg"
+  },
+  {
+    "revision": "c29c57227448183e34d34622fa7302a8",
+    "url": "fonts/able.c29c5722.eot"
+  },
+  {
+    "revision": "d10cfc08860bb646102a637d5f32bef7",
+    "url": "fonts/able.d10cfc08.ttf"
+  },
+  {
+    "revision": "4d59a9c7b243c7246cb579495d717431",
+    "url": "fonts/able.4d59a9c7.woff"
+  },
+  {
+    "revision": "370c0e97399485ea6c3c9fb6bff43405",
+    "url": "img/able.370c0e97.svg"
+  },
+  {
+    "revision": "d08cb9e058d1af7afc88b54b4cbc7a0d",
+    "url": "img/fertig.d08cb9e0.svg"
+  },
+  {
+    "revision": "9bcd8f07f26421e541bb3186c798c970",
+    "url": "index.html"
+  },
+  {
+    "revision": "7b7d6be66bc24110042dee55dd7b8994",
+    "url": "ap/build/ableplayer.min.css"
+  },
+  {
+    "revision": "754f949151bdbefe83e48df5cf6f0e2b",
+    "url": "ap/button-icons/able-icons.svg"
+  },
+  {
+    "revision": "8c1d595fb7de98b37744b85ed3ec7d28",
+    "url": "ap/button-icons/black/chapters.png"
+  },
+  {
+    "revision": "e165a87a1fc2761874543f1b91db6738",
+    "url": "ap/button-icons/black/captions.png"
+  },
+  {
+    "revision": "4791a06ce1c012d83ab2aa25865cf3dc",
+    "url": "ap/button-icons/black/close.png"
+  },
+  {
+    "revision": "31ffd6af6079b8b789ddfb7620a18431",
+    "url": "ap/build/ableplayer.min.js"
+  },
+  {
+    "revision": "fd7c427e7a9dce8d784bc9c7afde35d8",
+    "url": "ap/button-icons/black/descriptions.png"
+  },
+  {
+    "revision": "6440170a6a0c6ed63aa27ea6d5a17f39",
+    "url": "ap/button-icons/black/ellipsis.png"
+  },
+  {
+    "revision": "4d81b60f49705cb8fdeadc426dc8b0f0",
+    "url": "ap/button-icons/black/faster.png"
+  },
+  {
+    "revision": "27d50bde6f1cb158d29641235be6795e",
+    "url": "ap/button-icons/black/forward.png"
+  },
+  {
+    "revision": "34ed5fe6646062323d23714faa0b5301",
+    "url": "ap/button-icons/black/fullscreen-collapse.png"
+  },
+  {
+    "revision": "f89cc6d61b2523ee911c45e2985fcebd",
+    "url": "ap/button-icons/black/help.png"
+  },
+  {
+    "revision": "b027d08ce467d3b3b311effe0917cc0a",
+    "url": "ap/button-icons/black/fullscreen-expand.png"
+  },
+  {
+    "revision": "d8cb7342eeae4887687cfe57bf191004",
+    "url": "ap/button-icons/black/next.png"
+  },
+  {
+    "revision": "6d7f2f7223edf385f27dab3d0fddff4e",
+    "url": "ap/button-icons/black/pause.png"
+  },
+  {
+    "revision": "0286d90bbac8cd315818328cf7ac0b05",
+    "url": "ap/button-icons/black/pipe.png"
+  },
+  {
+    "revision": "c72b0066ef5d7a6ed7e9c79fb41da721",
+    "url": "ap/button-icons/black/play.png"
+  },
+  {
+    "revision": "4798a3ba49516470f7cd4905da668458",
+    "url": "ap/button-icons/black/previous.png"
+  },
+  {
+    "revision": "cd93c3986e4b75172562ae41f3e1b7da",
+    "url": "ap/button-icons/black/preferences.png"
+  },
+  {
+    "revision": "92a1b9055b5f0c4a2a58084ad292d91e",
+    "url": "ap/button-icons/black/rabbit.png"
+  },
+  {
+    "revision": "eec00795e82bf3172c404588b8fd1f6c",
+    "url": "ap/button-icons/black/rewind.png"
+  },
+  {
+    "revision": "f63b4a0efd8580eeb9a3bce027ab1e84",
+    "url": "ap/button-icons/black/restart.png"
+  },
+  {
+    "revision": "7e18cb0db0f4535f66ba935d8a56d276",
+    "url": "ap/button-icons/black/sign.png"
+  },
+  {
+    "revision": "405e4164f42e0f91e1b2ed1c7c5c87ce",
+    "url": "ap/button-icons/black/stop.png"
+  },
+  {
+    "revision": "b235bbb6e390aa09c73d1f30093bd168",
+    "url": "ap/button-icons/black/slower.png"
+  },
+  {
+    "revision": "e9ddc4ad9002b1cca95c0cb69dde48e3",
+    "url": "ap/button-icons/black/turtle.png"
+  },
+  {
+    "revision": "51c6843ec0572fc8eb00eb5588b11e79",
+    "url": "ap/button-icons/black/volume-loud.png"
+  },
+  {
+    "revision": "99e790b31e32ef6fd67246d438ca455c",
+    "url": "ap/button-icons/black/volume-medium.png"
+  },
+  {
+    "revision": "9c0cfc844787cb45f0a9830e3980f78f",
+    "url": "ap/button-icons/black/volume-mute.png"
+  },
+  {
+    "revision": "9d6758c92722535addfd8ba4697de28b",
+    "url": "ap/build/ableplayer.js"
+  },
+  {
+    "revision": "6d5c38f5093a292501468dab0e35f2d9",
+    "url": "ap/build/ableplayer.dist.js"
+  },
+  {
+    "revision": "4a125dc5dbcb5111680a212132bbd5f7",
+    "url": "ap/button-icons/black/volume-soft.png"
+  },
+  {
+    "revision": "7eec6190586457d91330dc02014687ce",
+    "url": "ap/button-icons/fonts/able.svg"
+  },
+  {
+    "revision": "c29c57227448183e34d34622fa7302a8",
+    "url": "ap/button-icons/fonts/able.eot"
+  },
+  {
+    "revision": "d10cfc08860bb646102a637d5f32bef7",
+    "url": "ap/button-icons/fonts/able.ttf"
+  },
+  {
+    "revision": "4d59a9c7b243c7246cb579495d717431",
+    "url": "ap/button-icons/fonts/able.woff"
+  },
+  {
+    "revision": "2f6f1aa4620c82503823ddffb6c66e9b",
+    "url": "ap/button-icons/white/captions.png"
+  },
+  {
+    "revision": "f689b02bde46001ce54840d22e0ea315",
+    "url": "ap/button-icons/white/chapters.png"
+  },
+  {
+    "revision": "3c9799e6f2d9a9347b0afc4bdff3e214",
+    "url": "ap/button-icons/black/transcript.png"
+  },
+  {
+    "revision": "4b344e11a1bb83bab86295c7f9cc7237",
+    "url": "ap/button-icons/white/close.png"
+  },
+  {
+    "revision": "eb32b3adddd848947943d5ed9a754a9a",
+    "url": "ap/button-icons/white/ellipsis.png"
+  },
+  {
+    "revision": "5daeaba03e52a0411c8d30a40afdab29",
+    "url": "ap/button-icons/white/descriptions.png"
+  },
+  {
+    "revision": "06a1ff70fbf65bf42874a92596b0d61a",
+    "url": "ap/button-icons/white/faster.png"
+  },
+  {
+    "revision": "e2408158f1cdb164e331ef24d6b59720",
+    "url": "ap/button-icons/white/forward.png"
+  },
+  {
+    "revision": "86fb61ec0345c0ec239131e14cf8335e",
+    "url": "ap/button-icons/white/fullscreen-collapse.png"
+  },
+  {
+    "revision": "bc0a92a7b62f71578ea5f15de498a99e",
+    "url": "ap/button-icons/white/help.png"
+  },
+  {
+    "revision": "5f1660f1ce37739a92258f973413389a",
+    "url": "ap/button-icons/white/next.png"
+  },
+  {
+    "revision": "ea6f69ef0f024a8ae590c271a99aa76e",
+    "url": "ap/button-icons/white/fullscreen-expand.png"
+  },
+  {
+    "revision": "4c6e30e1b1a4625e1112e53b1d50cbdc",
+    "url": "ap/button-icons/white/pause.png"
+  },
+  {
+    "revision": "e58724002730b64aee59ef3da90fffbb",
+    "url": "ap/button-icons/white/pipe.png"
+  },
+  {
+    "revision": "771170e0f50def53c3a3fa15b83b3008",
+    "url": "ap/button-icons/white/play.png"
+  },
+  {
+    "revision": "c582364a0f9833c2b170ddb2ae2c42f7",
+    "url": "ap/button-icons/white/preferences.png"
+  },
+  {
+    "revision": "ed2cdec93648c5050ceea23e834b7e99",
+    "url": "ap/button-icons/white/previous.png"
+  },
+  {
+    "revision": "c2183f8fadaa93bd34275793bec8b2b0",
+    "url": "ap/button-icons/white/rabbit.png"
+  },
+  {
+    "revision": "3abec70da682391c053c0b2fb9c47583",
+    "url": "ap/button-icons/white/restart.png"
+  },
+  {
+    "revision": "2b3895eedee377c2072c323df17c1b9b",
+    "url": "ap/button-icons/white/rewind.png"
+  },
+  {
+    "revision": "a9f41d0479de731c214af8ef944cfba2",
+    "url": "ap/button-icons/white/slower.png"
+  },
+  {
+    "revision": "fffd2f96858a473a6fa772e3a47fdb2b",
+    "url": "ap/button-icons/white/stop.png"
+  },
+  {
+    "revision": "7a302ee0160bf502d70265b3dd42f2db",
+    "url": "ap/button-icons/white/sign.png"
+  },
+  {
+    "revision": "b0affbc8c4757cec451f3c40dddebcf6",
+    "url": "ap/button-icons/white/transcript.png"
+  },
+  {
+    "revision": "9e436c2557d24c4695cbf6937bfcdca3",
+    "url": "ap/button-icons/white/turtle.png"
+  },
+  {
+    "revision": "02e2fc88f79cbe38f10cc8e71c491781",
+    "url": "ap/button-icons/white/volume-loud.png"
+  },
+  {
+    "revision": "2c29414b2dee7d11c3ef406e539f7286",
+    "url": "ap/button-icons/white/volume-medium.png"
+  },
+  {
+    "revision": "5af66128ca49abd37c20dc03581a472a",
+    "url": "ap/button-icons/white/volume-mute.png"
+  },
+  {
+    "revision": "a7488e9f064502f0d780cb642432a9e6",
+    "url": "ap/button-icons/white/volume-soft.png"
+  },
+  {
+    "revision": "fee78bf7dd88dcbd5fd3f72bcf420a37",
+    "url": "ap/images/wingrip.png"
+  },
+  {
+    "revision": "34ab3c4395592d053870f295cc17630c",
+    "url": "ap/LICENSE"
+  },
+  {
+    "revision": "60e63c7e8b9de5f9218efd857ecdb27d",
+    "url": "ap/thirdparty/js.cookie.js"
+  },
+  {
+    "revision": "2270268b63c7a1eb975058da92fea1b3",
+    "url": "ap/styles/ableplayer.css"
+  },
+  {
+    "revision": "51863b62bb326d9d68e53dc998c3f0d7",
+    "url": "ap/translations/ca.js"
+  },
+  {
+    "revision": "46718e87bea258618213910137d6413b",
+    "url": "ap/translations/de.js"
+  },
+  {
+    "revision": "a69e38564c1a0e8518a85ba333faf13d",
+    "url": "ap/translations/en.js"
+  },
+  {
+    "revision": "002c89ec281df9d23e194f852c5f04a6",
+    "url": "ap/translations/es.js"
+  },
+  {
+    "revision": "56273359318a4d3ae03773fb48f76423",
+    "url": "ap/translations/fr.js"
+  },
+  {
+    "revision": "c88fb991a4dc3dac3897b0878f202315",
+    "url": "ap/translations/he.js"
+  },
+  {
+    "revision": "64d66163a081e3e50dbc494791d30e01",
+    "url": "ap/translations/it.js"
+  },
+  {
+    "revision": "98c433e36eda236231a51780c2ffaa2f",
+    "url": "ap/translations/ja.js"
+  },
+  {
+    "revision": "f1eeb71f1bf8731062b89bbe507a4e04",
+    "url": "ap/translations/nb.js"
+  },
+  {
+    "revision": "a4944d3cd0370eb90d6c49c3af634583",
+    "url": "ap/translations/nl.js"
+  },
+  {
+    "revision": "99aa066f10d76d194af3d49edd6ecd69",
+    "url": "ap/translations/pt-br.js"
+  },
+  {
+    "revision": "ca0b1f734a988defa73693afc03cea10",
+    "url": "ap/translations/tr.js"
+  },
+  {
+    "revision": "d64d3b45dbf7ccd26d1f81650095f90a",
+    "url": "ap/translations/zh-tw.js"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  }
+];
