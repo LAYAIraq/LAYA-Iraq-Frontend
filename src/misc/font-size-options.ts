6180532605f03f28ebf0d7c6efa5647b7fa7e1f6
Constantin Mauf-Clausen <constantin.mauf@informatik.hu-berlin.de>
2022-02-08 22:19:26 +0000

    added missing dependency to fix pipeline

---
+++ src/misc/font-size-options.ts
@@ -0,0 +1,18 @@
+/**
+ * Filename: font-size-options.ts
+ * Use: Define selectable fonts and font sizes
+ * Creator: cmc
+ * Date: September 21, 2021
+ * Dependencies: none
+ */
+
+export default [
+  10,
+  12,
+  14,
+  16,
+  18,
+  20,
+  22,
+  24
+]
