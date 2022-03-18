97aeb1971556b8b6554071ea947c61847d71b61b
cmauf <constantin.mauf@outlook.de>
2022-03-18 13:43:45 +0100

    introduced getter for profile, removed console log from backend-url.ts, added tests to profile.spec.ts

--- tests/unit/views/profile.spec.ts
+++ tests/unit/views/profile.spec.ts
@@ -8,6 +8,7 @@ localVue.use(Vuex)
 
 describe('profile view', () => {
   let mutations
+  let state
   let actions
   let wrapper
   let vm
@@ -17,7 +18,7 @@ describe('profile view', () => {
       passwordRepeat: () => 'secret12',
       passwordSet: () => 'secret12'
     }
-    const state = {
+    state = {
       avatar: '',
       email: 'admin@laya',
       lang: 'en',
@@ -54,6 +55,11 @@ describe('profile view', () => {
       modules: {
         profile: {
           state,
+          getters: {
+            profile () {
+              return state
+            }
+          },
           mutations,
           actions
         }
@@ -61,11 +67,12 @@ describe('profile view', () => {
     })
 
     wrapper = shallowMount(ProfileView, {
-      computed: {
-        passwordOk () {
-          return true
+      data() {
+        return {
+          passwordOk: true
         }
       },
+      localVue,
       mocks: {
         $bvToast: {
           show: jest.fn()
@@ -73,14 +80,65 @@ describe('profile view', () => {
       },
       store,
       stubs: [
+        'b-form-input',
+        'b-form-select',
+        'b-form-select-option',
         'b-toast',
         'password-input'
-      ]
+      ],
+      watch: {
+        profile: () => jest.fn()
+      }
     })
     vm = wrapper.vm as any
   })
 
-  it('', () => {
+  it('saves media input as chosen', async () => {
+    const button = wrapper.find('button')
+    await button.trigger('click')
+    expect(mutations.setPrefs).toHaveBeenCalledWith(
+      expect.objectContaining({ id: 1 }), expect.objectContaining({
+      media: {
+        text: true,
+        video: true,
+        audio: true,
+        simple: true
+      }
+    }))
+    const inputFields = wrapper.findAll('input').filter(elem => elem.attributes('type') === 'checkbox')
+    inputFields.setChecked(false)
+    await button.trigger('click')
+    expect(mutations.setPrefs).toHaveBeenLastCalledWith(
+      expect.objectContaining({ id: 1 }), expect.objectContaining({
+        media: {
+          text: false,
+          video: false,
+          audio: false,
+          simple: false
+        }
+      }))
+    inputFields.at(1).setChecked(true)
+    inputFields.at(3).setChecked(true)
+    await button.trigger('click')
+    expect(mutations.setPrefs).toHaveBeenLastCalledWith(
+      expect.objectContaining({ id: 1 }), expect.objectContaining({
+        media: {
+          text: false,
+          video: false,
+          audio: true,
+          simple: true
+        }
+      }))
+    expect(actions.saveProfile).toHaveBeenCalled()
+    expect(actions.changePassword).toHaveBeenCalled()
+    // expect(actions.saveProfile).toHaveBeenCalledTimes(3)
+  })
 
+  it('disables button when password input wrong', async () => {
+    const button = wrapper.find('button')
+    expect(button.attributes('disabled')).toBeUndefined()
+    vm.passwordOk = false
+    await vm.$nextTick()
+    expect(button.attributes('disabled')).toBeDefined()
   })
 })
