f76d5d774ec3f3f8470d90b1d8cfc2eddf1fa93f
cmauf <constantin.mauf@outlook.de>
2022-03-21 21:45:32 +0100

    updated register.spec.ts for new component

--- tests/unit/views/register.spec.ts
+++ tests/unit/views/register.spec.ts
@@ -43,50 +43,69 @@ describe('register component', () => {
     })
     vm = wrapper.vm as any
     nameInput = wrapper.find('#name-input')
-    nameError = wrapper.find('#name-err')
     emailInput = wrapper.find('#email-input')
     emailError = wrapper.find('#email-err')
   })
 
+  it('no error when empty name but no input', () => {
+    const nameEmptyError = wrapper.find('#name-empty-err')
+    expect(nameEmptyError.exists()).toBeFalsy()
+  })
+
+  it('error when empty name but input has started', async () => {
+    await nameInput.setValue('someName')
+    await nameInput.setValue('')
+    const nameEmptyError = wrapper.find('#name-empty-err')
+    expect(nameEmptyError.exists()).toBeTruthy()
+  })
+
   it('error when name has space', async () => {
     await nameInput.setValue('some name')
-    expect(nameError.attributes('style')).toBe('')
+    nameError = wrapper.find('#name-err')
+    expect(nameError.exists()).toBeTruthy()
     await nameInput.setValue('some\tname')
-    expect(nameError.attributes('style')).toBe('')
+    nameError = wrapper.find('#name-err')
+    expect(nameError.exists()).toBeTruthy()
   })
 
   it('error when name has other than numbers and digits', async () => {
     await nameInput.setValue('?!jhasdb')
-    expect(nameError.attributes('style')).toBe('')
-    await nameInput.setValue('()')
-    expect(nameError.attributes('style')).toBe('')
-    await nameInput.setValue('exMarxTheSpot!')
-    expect(nameError.attributes('style')).toBe('')
+    nameError = wrapper.find('#name-err')
+    expect(nameError.exists()).toBeTruthy()
+  })
+
+  it('error when non-utf8 signs', async () => {
     await nameInput.setValue('こんばんは')
-    expect(nameError.attributes('style')).toBe('')
+    nameError = wrapper.find('#name-err')
+    expect(nameError.exists()).toBeTruthy()
+  })
+
+  it('error when emoji input', async () => {
     await nameInput.setValue('💭❤💞💯📘🎹⚽🍊😅🎃😻👢☂🌸⛄⭐🙈🍍☕🚚🏰👣')
-    expect(nameError.attributes('style')).toBe('')
+    nameError = wrapper.find('#name-err')
+    expect(nameError.exists()).toBeTruthy()
   })
 
   it('no error message when name has only letters', async () => {
     await nameInput.setValue('SomeName')
-    expect(nameError.attributes('style')).toBe('display: none;')
+    nameError = wrapper.find('#name-err')
+    expect(nameError.exists()).toBeFalsy()
     await nameInput.setValue('somename')
-    expect(nameError.attributes('style')).toBe('display: none;')
+    expect(nameError.exists()).toBeFalsy()
   })
 
-  it('no error when name has digits', async () => {
-    await nameInput.setValue('50343')
-    expect(nameError.attributes('style')).toBe('display: none;')
+  it('no error when name has only digits', async () => {
     await nameInput.setValue('1234567890')
-    expect(nameError.attributes('style')).toBe('display: none;')
+    nameError = wrapper.find('#name-err')
+    expect(nameError.exists()).toBeFalsy()
   })
 
   it('no error when name has digits and characters combined', async () => {
     await nameInput.setValue('50m3n4m3')
-    expect(nameError.attributes('style')).toBe('display: none;')
+    nameError = wrapper.find('#name-err')
+    expect(nameError.exists()).toBeFalsy()
     await nameInput.setValue('bms1234567890sdv')
-    expect(nameError.attributes('style')).toBe('display: none;')
+    expect(nameError.exists()).toBeFalsy()
   })
 
   it('error when wrong email address', async () => {
@@ -118,19 +137,22 @@ describe('register component', () => {
     await emailInput.setValue('correct@example.com')
     const sendButton = wrapper.find('button')
     expect(sendButton).toBeDefined()
+    expect(sendButton.attributes('disabled')).toBeUndefined()
   })
 
   it('doesn\'t show button with wrong name', async () => {
     await nameInput.setValue('wrong Name')
     await emailInput.setValue('correct@example.com')
     const sendButton = wrapper.find('button')
-    expect(sendButton.exists()).toBeFalsy()
+    expect(sendButton).toBeDefined()
+    expect(sendButton.attributes('disabled')).toBe('disabled')
   })
 
   it('doesn\'t show button with wrong email', async () => {
     await nameInput.setValue('CorrectName')
     await emailInput.setValue('wrongexample.com')
     const sendButton = wrapper.find('button')
-    expect(sendButton.exists()).toBeFalsy()
+    expect(sendButton).toBeDefined()
+    expect(sendButton.attributes('disabled')).toBe('disabled')
   })
 })
