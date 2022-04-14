d6f3a6ae6af5abf43ba66eb71e75e0f8b2bc00ca
cmauf <constantin.mauf@outlook.de>
2022-04-14 23:22:34 +0200

    added header.spec.ts

---
+++ tests/unit/components/header.spec.ts
@@ -0,0 +1,142 @@
+import { createLocalVue, mount } from '@vue/test-utils'
+import Header from '@/components/header.vue'
+import BootstrapVue from 'bootstrap-vue'
+import Vuex from 'vuex'
+import 'regenerator-runtime/runtime'
+
+const localVue = createLocalVue()
+localVue.use(BootstrapVue)
+localVue.use(Vuex)
+
+describe ('Header unauthorized', () => {
+  let wrapper
+  let actions
+  beforeEach (() => {
+    const getters = {
+      profileLang: () => 'en'
+    }
+    const auth = {
+      state: {
+        online: false
+      }
+    }
+    actions = {
+      getBrowserLocale: jest.fn(() => Promise.resolve())
+    }
+    const store = new Vuex.Store({
+      getters,
+      modules: {
+        auth
+      }
+    })
+    wrapper = mount(Header, {
+      store,
+      localVue
+    })
+  })
+
+  it ('has 4 links', async () => {
+    const links = wrapper.findAll('a')
+    expect(links.length).toBe(4)
+  })
+
+})
+
+describe ('Header authorized', () => {
+  let wrapper
+  let mutations
+  let actions
+  beforeEach (() => {
+    const getters = {
+      isAdmin: () => false,
+      messages: () => [],
+      profileLang: () => 'en',
+      unreadMessages: () => 0
+    }
+    mutations = {
+      logout: jest.fn(),
+      setLang: jest.fn()
+    }
+    actions = {
+      getBrowserLocale: jest.fn(() => Promise.reject()),
+      setUserLang: jest.fn()
+    }
+    const auth = {
+      state: {
+        online: true
+      }
+    }
+    const store = new Vuex.Store({
+      actions,
+      getters,
+      mutations,
+      modules: {
+        auth
+      }
+    })
+    wrapper = mount(Header, {
+      mocks: {
+        $ls: {
+          remove: jest.fn()
+        },
+        $router: {
+          push: jest.fn()
+        }
+      },
+      store,
+      stubs: ['ly-header-notifications'],
+      localVue
+    })
+  })
+
+  it ('has 5 links', async () => {
+    const links = wrapper.findAll('a')
+    expect(links.length).toBe(5)
+  })
+
+  it ('triggers logout and redirects when clicked', async () => {
+    const logoutAnchor = wrapper.findAll('a').wrappers[3]
+    expect(logoutAnchor.text()).toBe('Logout')
+    await logoutAnchor.trigger('click')
+    expect(wrapper.vm.$ls.remove).toHaveBeenCalled()
+    expect(wrapper.vm.$router.push).toHaveBeenCalledWith('/login')
+    expect(mutations.logout).toHaveBeenCalled()
+  })
+
+  it ('fires changes language request', async () => {
+    const langButtons = wrapper.find('.dropdown-menu').findAll('button')
+    await langButtons.wrappers.forEach(wrapper => wrapper.trigger('click'))
+    expect(mutations.setLang).toHaveBeenCalledTimes(4)
+    expect(actions.setUserLang).toHaveBeenCalledTimes(4)
+  })
+})
+
+describe ('Header admin', () => {
+  it('has 6 links', () => {
+    const getters = {
+      profileLang: () => 'en',
+      isAdmin: () => true
+    }
+    const actions = {
+      getBrowserLocale: () => Promise.resolve()
+    }
+    const auth = {
+      state: {
+        online: true
+      }
+    }
+    const store = new Vuex.Store({
+      actions,
+      getters,
+      modules: {
+        auth
+      }
+    })
+    const wrapper = mount(Header, {
+      store,
+      stubs: ['ly-header-notifications'],
+      localVue
+    })
+    expect(wrapper.findAll('a').length).toBe(6)
+  })
+})
