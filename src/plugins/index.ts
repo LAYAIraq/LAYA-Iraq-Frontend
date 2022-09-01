982ba29909ea38feae26d3e84f28eebf8d5ba944
cmauf <constantin.mauf@outlook.de>
2022-09-01 11:14:38 +0200

    lint fix

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -10,7 +10,7 @@ export { default as LayaScMc } from './laya-la-scmc'
 // export { default as LayaQuizSort } from './laya-la-sort'
 export { default as LayaQuizDragDrop } from './laya-la-drag-drop'
 export { default as LayaQuizRelate } from './laya-la-relate'
-export { default as LayaFeedback} from './laya-la-feedback'
+export { default as LayaFeedback } from './laya-la-feedback'
 
 // Course Content Blocks
 export { default as LayaDialog } from './laya-lb-dialog'
