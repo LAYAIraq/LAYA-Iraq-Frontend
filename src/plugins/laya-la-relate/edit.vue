53acbc23f8ae59a02eda0bfb4ed892d396867fe5
cmauf <constantin.mauf@outlook.de>
2023-01-11 14:21:01 +0100

    TLI-138 updated edit, view component

--- src/plugins/laya-la-relate/edit.vue
+++ src/plugins/laya-la-relate/edit.vue
@@ -165,7 +165,7 @@ Dependencies:
           <div class="col">
             <input
               :id="'rel-text-'+i"
-              v-model="relations[i]"
+              v-model="rel"
               class="form-control"
               type="text"
             >
@@ -323,7 +323,7 @@ Dependencies:
 
 <script>
 import { mapGetters } from 'vuex'
-import { locale, tooltipIcon } from '@/mixins'
+import { locale, routeProps, tooltipIcon } from '@/mixins'
 import commonMethods from '@/plugins/laya-la-relate/common-methods'
 
 export default {
@@ -332,6 +332,7 @@ export default {
   mixins: [
     commonMethods,
     locale,
+    routeProps,
     tooltipIcon
   ],
 
@@ -347,7 +348,7 @@ export default {
   },
 
   computed: {
-    ...mapGetters(['content'])
+    ...mapGetters(['courseContent', 'courseSimple'])
   },
 
   created () {
@@ -363,8 +364,7 @@ export default {
      * Last Updated: March 19, 2021
      */
     fetchData () {
-      const idx = this.$route.params.step - 1
-      const preData = JSON.parse(JSON.stringify(this.content[idx].input))
+      const preData = JSON.parse(JSON.stringify(this.courseContent[this.pathId]))
       this.title = preData.title
       this.task = preData.task
       this.taskAudio = preData.taskAudio
--- src/plugins/laya-la-relate/view.vue
+++ src/plugins/laya-la-relate/view.vue
@@ -136,9 +136,24 @@ Dependencies:
         >
           {{ y18n('check') }}
         </button>
+        <button
+          type="button"
+          class="btn btn-primary mt-3 btn-lg"
+          :class="langIsAr? 'float-left mr-auto': 'float-right ml-auto'"
+          @click="done"
+        >
+          <span>
+            {{ y18n('nextContent') }}
+            <i
+              :class="langIsAr?
+                'fas fa-arrow-left' :
+                'fas fa-arrow-right'"
+            ></i>
+          </span>
+        </button>
       </div>
 
-      <div>
+      <div class="mt-3">
         <div class="row">
           <div
             v-if="showSolutionsBool"
@@ -170,21 +185,7 @@ Dependencies:
             {{ y18n('layaLaRelate.missingAnswerWarning') }}
           </b-modal>
         </div>
-        <button
-          type="button"
-          class="btn btn-primary btn-lg mt-3 d-block"
-          :class="langIsAr? 'float-left mr-auto': 'float-right ml-auto'"
-          @click="done"
-        >
-          <span>
-            {{ y18n('nextContent') }}
-            <i
-              :class="langIsAr?
-                'fas fa-arrow-left' :
-                'fas fa-arrow-right'"
-            ></i>
-          </span>
-        </button>
+
       </div>
     </div>
   </div>
@@ -192,7 +193,7 @@ Dependencies:
 
 <script>
 import { mapGetters } from 'vuex'
-import { locale, viewPluginProps, watchContent } from '@/mixins'
+import { locale, viewPluginProps } from '@/mixins'
 import '@/styles/flaggables.css'
 
 export default {
@@ -200,39 +201,23 @@ export default {
 
   mixins: [
     locale,
-    viewPluginProps,
-    watchContent
+    viewPluginProps
   ],
 
   data () {
-    if (this.viewData) { // preview
-      return {
-        ...this.viewData,
-        defaultOption: '',
-        solution: [],
-        eval: [],
-        freeze: false
-      }
-    }
     return {
+      ...this.viewData,
       allAnswersChosen: false,
       defaultOption: '',
+      solution: [],
       eval: [],
       freeze: false,
-      pairs: [],
-      relations: [],
-      relationsSimple: [],
-      showSolutionsBool: false,
-      solution: [],
-      task: {},
-      taskAudio: '',
-      title: {}
+      showSolutionsBool: false
     }
   },
 
   computed: {
-    ...mapGetters(['content',
-      'courseSimple']),
+    ...mapGetters(['courseSimple']),
 
     /**
      * options: map pairs to their relation
@@ -274,9 +259,6 @@ export default {
 
   created () {
     this.defaultOption = this.y18n('layaLaRelate.defaultOption')
-    if (!this.viewData) { // no preview
-      this.fetchData()
-    }
     // eslint-disable-next-line no-unused-vars
     for (const i in this.pairs) {
       this.solution.push(-1)
@@ -339,24 +321,6 @@ export default {
         this.allAnswersChosen = true
         this.$bvModal.show('relate-missing-warning')
       }
-    },
-
-    /**
-     * Function fetchData: fetch data from vuex and create data property
-     *
-     * Author: cmc
-     *
-     * Last Updated: March 03, 2022
-     */
-    fetchData () {
-      const idx = this.$route.params.step - 1
-      const preData = JSON.parse(JSON.stringify(this.content[idx].input))
-      this.relations = preData.relations
-      this.relationsSimple = preData.relationsSimple
-      this.title = preData.title
-      this.task = preData.task
-      this.taskAudio = preData.taskAudio
-      this.pairs = preData.pairs
     }
   }
 }
