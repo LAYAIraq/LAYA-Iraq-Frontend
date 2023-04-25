6af09d70961ab66977f7fa145838fd33c7d0e3c2
Nicole Vieregg <n.vieregg@gmail.com>
2023-04-25 10:25:43 +0200

    TLI-211 refactored institutions.ts and occupations.ts

--- src/misc/occupations.ts
+++ src/misc/occupations.ts
@@ -2,14 +2,13 @@
  * Filename: occupations.ts
  * Use: Define selectable occupations in profile
  * Creator: nv
- * Date: November 25, 2022
- * Dependencies: none
+ * Since: 1.3.0
  */
 export default [
-  { value: 'lecturer', text: 'Lecturer' },
-  { value: 'professor', text: 'Professor' },
-  { value: 'researcher', text: 'Researcher' },
-  { value: 'teacher', text: 'Teacher' },
-  { value: 'student', text: 'Student' },
-  { value: 'admin', text: 'Administrator' }
+  'lecturer',
+  'professor',
+  'researcher',
+  'teacher',
+  'student',
+  'admin'
 ]
