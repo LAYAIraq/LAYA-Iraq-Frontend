f246ac1d449fe62eaad5530f8cafb231264baa7d
Nicole Vieregg <n.vieregg@gmail.com>
2023-09-25 16:19:43 +0200

    TLI-6 added plugin files to add certificate to a course

---
+++ src/plugins/organization/certificate/certificate-edit.vue
@@ -0,0 +1,163 @@
+<!--
+Filename: certificate-edit.vue
+Use: Edit certificate content block
+Creator: nv
+Since: v1.3.0
+-->
+
+<template>
+  <div
+    :class="langIsAr? 'text-right' : 'text-left'"
+  >
+    <div class="row">
+      <h2 class="d-inline-block mr-auto">
+        {{ y18n('certificate.name') }}
+      </h2>
+
+      <i
+        id="questionmark"
+        v-b-tooltip.left
+        class="fas fa-question-circle"
+        :class="langIsAr? 'mr-auto' : 'ml-auto'"
+        :title="y18n('showTip')"
+        aria-labelledby="tooltipText"
+        aria-live="polite"
+        @click="toggleTip"
+      ></i>
+    </div>
+    <hr>
+
+    <b-jumbotron
+      v-if="tooltipOn"
+      id="tooltipText"
+      :header="y18n('certificate.name')"
+      :lead="y18n('tipHeadline')"
+    >
+      <hr class="my-4">
+      <span>
+        {{ y18n('certificate.tooltip') }}
+      </span>
+    </b-jumbotron>
+
+    <form>
+      <div class="form-group">
+        <div class="row">
+          <!-- additional information about course -->
+          <label
+            for="additional-information"
+            class="col-2 col-form-label"
+          >
+            {{ y18n('certificate.additionalInformation') }}
+          </label>
+          <div class="col-8">
+            <input
+              id="additional-information"
+              v-model="information.text"
+              type="text"
+              class="form-control"
+              :placeholder="y18n('certificate.optional')"
+              :aria-label="y18n('certificate.optional')"
+            >
+          </div>
+        </div>
+        <!-- simple additional information -->
+        <div
+          v-if="courseSimple"
+          class="row"
+        >
+          <label
+            for="additional-information-simple"
+            class="col-2 col-form-label"
+          >
+            <span class="sr-only">
+              {{ y18n('simpleAlt') }}
+            </span>
+          </label>
+          <div class="col-8">
+            <input
+              id="additional-information-simple"
+              v-model="information.simple"
+              type="text"
+              class="form-control"
+              :placeholder="y18n('simpleAlt')"
+            >
+          </div>
+        </div>
+      </div>
+      <div class="form-group">
+        <!-- signature -->
+        <div class="row">
+          <label
+            for="signature"
+            class="col-2 col-form-label"
+          >
+            {{ y18n('certificate.signature') }}
+          </label>
+          <div class="col-10">
+            <textarea
+              id="signature"
+              v-model="signature.image"
+              class="w-100"
+              :placeholder="y18n('certificate.optional')"
+            ></textarea>
+          </div>
+        </div>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { locale, pluginEdit, tooltipIcon } from '@/mixins'
+import { mapGetters } from 'vuex'
+import { deepCopy } from '@/mixins/general/helpers'
+
+export default {
+  name: 'CertificateEdit',
+
+  mixins: [
+    locale,
+    pluginEdit,
+    tooltipIcon
+  ],
+
+  data () {
+    return {
+      signature: '',
+      information: {}
+    }
+  },
+
+  computed: {
+    ...mapGetters(['courseContent', 'courseSimple'])
+  },
+
+  created () {
+    if (this.edit) {
+      this.fetchData()
+    } else {
+      this.taskTitlePopulate()
+    }
+  },
+
+  methods: {
+    /**
+     * Function fetchData: Fetch data from vuex and make data property
+     *
+     * Author: cmc
+     *
+     * Last Updated: March 19, 2021
+     */
+    fetchData () {
+      // create deep copy of store object to manipulate in vue instance
+      const preData = deepCopy(this.courseContent[this.pathId])
+      this.information = preData.information
+      this.signature = preData.signature
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/plugins/organization/certificate/certificate-view.vue
@@ -0,0 +1,49 @@
+<!--
+Filename: certificate-view.vue
+Use: View certificate content block
+Author: core
+Since v1.0.0
+-->
+
+<template>
+  <fieldset
+    class="certificate-view"
+  >
+    <div>
+      <!-- add pdf viewer -->
+    </div>
+  </fieldset>
+</template>
+
+<script>
+
+import { locale, pluginView } from '@/mixins'
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'CertificateView',
+
+  mixins: [
+    locale,
+    pluginView
+  ],
+
+  data () {
+    return {
+      ...this.viewData
+    }
+  },
+
+  computed: {
+    ...mapGetters([
+      'courseFlags',
+      'courseSimple'
+    ])
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
