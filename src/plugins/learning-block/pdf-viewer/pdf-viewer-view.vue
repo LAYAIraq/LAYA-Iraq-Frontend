a6c04a508223a59fdbd78206d0174e17bb89a316
Nicole Vieregg <n.vieregg@gmail.com>
2023-10-18 12:12:08 +0200

    TLI-188 made a new pdf viewer branch from th content restructure branch as the old one was super outdated, just transferred the already prepared files and imports

---
+++ src/plugins/learning-block/pdf-viewer/pdf-viewer-view.vue
@@ -0,0 +1,93 @@
+<!--
+Filename: pdf-text-view.vue
+Use: View pdf viewer content block
+Creator: nv
+Since: v1.3.0
+-->
+
+<template>
+  <div
+    class="pdf-viewer-view"
+    :class="langIsAr? 'text-right' : 'text-left'"
+  >
+    <div
+      v-if="title.show"
+      :id="title.id"
+      class="flaggable row"
+    >
+      <h2>{{ courseSimple? title.simple : title.text }}</h2>
+      <flag-icon
+        v-if="!editPreview"
+        :ref-data="title"
+        @flagged="title.flagged = true"
+      ></flag-icon>
+    </div>
+    <div>
+      <vue-pdf-embed>
+        :src="src"
+      </vue-pdf-embed>
+    </div>
+    <div class="row">
+      <button
+        type="button"
+        class="btn btn-primary mt-3 d-block"
+        :class="langIsAr? 'float-left mr-auto': 'float-right ml-auto'"
+        @click="onFinish[0]() || {}"
+      >
+        {{ y18n('nextContent') }}
+        <i
+          :class="langIsAr?
+            'fas fa-arrow-left':
+            'fas fa-arrow-right'"
+        ></i>
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+import { locale, pluginView } from '@/mixins'
+import '@/assets/styles/flaggables.css'
+import FlagIcon from '@/components/course/flag/flag-icon.vue'
+import { mapGetters } from 'vuex'
+import VuePdfEmbed from 'vue-pdf-embed/dist/vue2-pdf-embed'
+
+export default {
+  name: 'PdfViewerView',
+  components: { FlagIcon, VuePdfEmbed },
+
+  mixins: [
+    locale,
+    pluginView
+  ],
+
+  data () {
+    return {
+      ...this.viewData,
+      src: 'https://www.uni-flensburg.de/fileadmin/content/seminare/medienbildung/dokumente/projekte/dig-in/handreichung-inklusiv-digitale-schul-und-unterrichtsentwicklung-wie-gelingt-das.pdf'
+    }
+  },
+
+  computerd: {
+    ...mapGetters([
+      'courseContent',
+      'courseSimple'
+    ])
+  },
+
+  mounted () {
+    this.initPdfViewer()
+  },
+
+  methods: {
+
+    initPdfViewer () {
+
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
