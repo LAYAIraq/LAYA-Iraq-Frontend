d117e9f3cc8759493b1e4f79541e56ba6b043d13
NVieregg <97884768+NVieregg@users.noreply.github.com>
2023-12-20 20:46:13 +0100

    Dev/tli 188 pdf viewer plugin new (#363)
    * TLI-188 made a new pdf viewer branch from th content restructure branch as the old one was super outdated, just transferred the already prepared files and imports
    
    * TLI-188 added i18n en entries
    
    * TLI-188 minor changes in comments and fix in view to correctly read source; vue-pdf-embed throws error, but no documentation found
    
    * TLI-188 minor changes for rendering, still not working
    
    * TLI-188 added pdf-viewer with pdfjs
    
    * TLI-188 removed past css style definition, changed fallback pdf to correct name
    
    * TLI-188 added stackoverflow solution, but still no compiling due to loader error
    
    * TLI-188 added working pdf viewer component and download button
    
    * TLI-188 fixed missing title in add new content block
    
    * TLI-188 centered the pdf viewer and the download button
    
    * TLI-188 fixed mobile display error
    
    * TLI-188 fix bug in block save, made view use NavigationButtons
    
    ---------
    
    Co-authored-by: cmauf <constantin.mauf@outlook.de>

---
+++ src/components/helpers/pdf-viewer.vue
@@ -0,0 +1,81 @@
+<!--
+Filename: pdf-viewer.vue
+Use: initializes pdf viewer
+Creator: nv
+Since: v1.3.0
+-->
+
+<template>
+  <div>
+  </div>
+</template>
+
+<script>
+import { getDocument, GlobalWorkerOptions as pdfworkerOpts } from 'pdfjs-dist/legacy/build/pdf'
+import { locale } from '@/mixins'
+
+export default {
+  name: 'PdfViewer',
+
+  mixins: [
+    locale
+  ],
+  props: {
+    url: {
+      type: String,
+      default: 'https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf'
+    }
+  },
+
+  data () {
+    return {
+
+    }
+  },
+
+  created () {
+  },
+
+  mounted () {
+    pdfworkerOpts.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/2.16.105/pdf.worker.js'
+
+    // console.log('pdfUrl ' + this.url)
+    const loadingTask = getDocument({
+      url: this.url
+    })
+    loadingTask.promise.then(pdf => {
+      // Render each page
+      for (let pageNum = 1; pageNum <= pdf.numPages; pageNum++) {
+        pdf.getPage(pageNum).then(page => {
+          const scale = 1.5
+          const viewport = page.getViewport({ scale: scale })
+
+          // Prepare canvas for each page
+          const canvas = document.createElement('canvas')
+          const context = canvas.getContext('2d')
+          canvas.height = viewport.height
+          canvas.width = viewport.width
+
+          // Append canvas to the component
+          this.$el.appendChild(canvas)
+
+          // Render page into canvas
+          const renderContext = {
+            canvasContext: context,
+            viewport: viewport
+          }
+          page.render(renderContext).promise.then(() => {
+            // console.log(`Page ${pageNum} rendered`)
+          })
+        })
+      }
+    }, reason => {
+      console.error(reason)
+    })
+  }
+
+}
+</script>
+
+<style>
+</style>
